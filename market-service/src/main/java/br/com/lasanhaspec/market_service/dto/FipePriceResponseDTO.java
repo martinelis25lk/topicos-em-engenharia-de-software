bd733961acb221f21ced01a3683eaa7d8e4c67bc
martinelis25lk <gui.almeida.edu@gmail.com>
2026-05-06 13:06:45 -0300

    wip: creating atributes to ExternalFipeRespondeDTO

--- market-service/src/main/java/br/com/lasanhaspec/market_service/dto/FipePriceResponseDTO.java
+++ market-service/src/main/java/br/com/lasanhaspec/market_service/dto/FipePriceResponseDTO.java
@@ -1,15 +1,21 @@
 package br.com.lasanhaspec.market_service.dto;
 
-
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+//resposta mais limpa do market-servisos
+
+
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class FipePriceResponseDTO {
-    private String Valor;
-    private String Marca;
-    private String Modelo;
-    private String AnoModelo;
-    private String Combustivel;
-    private String CodigoFipe;
-    private String MesReferencia;
+    private String valor;
+    private String marca;
+    private String modelo;
+    private String anoModelo;
+    private String combustivel;
+    private String codigoFipe;
+    private String mesReferencia;
 }
