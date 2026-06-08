46da872cf720cb2b4ed2acd582b468178256eb8d
Joao <joao2015.76@gmail.com>
2026-06-08 13:11:04 -0300

    feature: adding user update prof pic

--- car-service/src/main/java/br/com/lasanhaspec/carservice/infrastructure/storage/S3StorageService.java
+++ car-service/src/main/java/br/com/lasanhaspec/carservice/infrastructure/storage/S3StorageService.java
@@ -1,51 +1,53 @@
 package br.com.lasanhaspec.carservice.infrastructure.storage;
 
-
-
-
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 
 import java.io.IOException;
 import java.util.UUID;
 
 @Service
 @Slf4j
-public class S3StorageService implements  StorageService{
+public class S3StorageService implements StorageService {
 
     private final S3Client s3Client;
     private final String bucketName;
 
+    public S3StorageService() {
 
+        AwsBasicCredentials credentials =
+                AwsBasicCredentials.create(
+                        "AKIA2EVMOFZAOVH66SHX",
+                        "yiqjWFYJ7hlSmgkuAFhOWP9w45K0wfltt3kuL3zW"
+                );
 
-    public S3StorageService(){
         this.bucketName = "lasanhaspec-vehicle-images-dev";
-        this.s3Client   = S3Client.builder()
+
+        this.s3Client = S3Client.builder()
                 .region(Region.of("us-east-2"))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(credentials)
+                )
                 .build();
     }
 
-
-
-
-
     @Override
-    public UploadResult uploadFile(MultipartFile file){
+    public UploadResult uploadFile(MultipartFile file) {
 
-        String fileName = UUID.randomUUID() + "-" + file.getOriginalFilename().replace(" ", "_");
+        String fileName =
+                UUID.randomUUID() + "-" +
+                        file.getOriginalFilename().replace(" ", "_");
 
-        //String fileName = UUID.randomUUID() + "-shmalugou-" + file.getOriginalFilename();
-
-
-        try{
+        try {
             s3Client.putObject(
                     PutObjectRequest.builder()
                             .bucket(bucketName)
@@ -55,33 +57,29 @@ public class S3StorageService implements  StorageService{
                     RequestBody.fromBytes(file.getBytes())
             );
         } catch (IOException e) {
-            log.info(e.getMessage());
-            throw new RuntimeException("Error during upload service kkkk S3StorageService", e);
-        } catch (Exception e){
-            log.info(e.getMessage());
+            throw new RuntimeException("Erro ao enviar arquivo", e);
         }
 
-        String url = "https://" + bucketName + ".s3.amazonaws.com/" + fileName;
+        String url =
+                "https://" +
+                        bucketName +
+                        ".s3.amazonaws.com/" +
+                        fileName;
 
         return new UploadResult(url, fileName);
-
     }
 
-
     @Override
-    public void deleteFile(String fileKey){
+    public void deleteFile(String fileKey) {
         s3Client.deleteObject(
                 DeleteObjectRequest.builder()
                         .bucket(bucketName)
                         .key(fileKey)
                         .build()
-
-
         );
     }
 
-
-    public class UploadResult {
+    public static class UploadResult {
 
         private final String url;
         private final String key;
